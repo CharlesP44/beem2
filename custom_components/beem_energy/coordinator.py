94ac550c0df971db50f6fefb5057a2b23bc3facf
Charles P <charles.picaud39@gmail.com>
2025-10-25 17:25:00 +0200

    Update coordinator.py

--- custom_components/beem_energy/coordinator.py
+++ custom_components/beem_energy/coordinator.py
@@ -10,17 +10,10 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN, BASE_URL
-from .beem_api import get_devices, get_tokens, get_battery_live_data
 from .exceptions import BeemAuthError, BeemConnectionError
 from .utils.merge import merge_live_into_battery
 from datetime import datetime, timezone
-from .beem_api import (
-    get_devices,
-    get_tokens,
-    get_battery_live_data,
-    get_box_summary,
-    get_battery_control_parameters,
-)
+from .beem_api import get_devices, get_tokens, get_battery_live_data, get_box_summary, get_battery_control_parameters
 
 
 _LOGGER = logging.getLogger(__name__)
